8309b86cf54704d5bc580c026cd9065110c0bde8
root <jtm5432@naver.com>
2023-12-10 12:30:40 +0900

    app.tsx에 provider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,13 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { QueryClient } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 
 import 'tailwindcss/tailwind.css'; // tailwind styles
-
+export function createQueryClient() {
+  return new QueryClient();
+}
 // 테마 색상 정의
 const theme = {
   colors: {
@@ -37,11 +41,17 @@ html {
 `;
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const queryClient = createQueryClient();
+
   return (
+    <QueryClientProvider client={queryClient}>
+
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
+    </QueryClientProvider>
+
   );
 }
 
